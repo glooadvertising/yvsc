32925c691a178ab577bfaa8859686579f542d1b5
Pablo Sazo <pablo@glooadvertising.com.au>
2023-11-17 16:18:44 +1100

    full home page

--- front-page.php
+++ front-page.php
@@ -12,7 +12,7 @@
             <span>
                 <?php the_content(); ?>
             </span>
-            <a href="#" class="btn">
+            <a href="#" class="btn btn-fwd">
             
                 <?php the_title(); ?>
             </a>
@@ -32,21 +32,21 @@
         <div class="row">
             <div class="grey-block-left round"></div>
             <div class="centered-content mission round">
-                <h3>Mission</h3>
+                <h2>Mission</h2>
                 <p>To work with and alongside people to support them to fulfil their goals to enjoy their life. We are driven with a person-centred passion to contribute and make a difference in issues that matter to the community, both individually and collectively.</p>
             </div>
-            <img src="<?php echo get_template_directory_uri();?>/assets/images/iso.png" alt="Mission" class="round">
+            <img src="http://yvsc.test/wp-content/uploads/2023/11/stock-photo-down-syndrome-man-attending-education-class-in-community-center-inclusivity-of-disabled-person-1974822152.jpg" alt="Mission" class="round">
         </div>
         <div class="row">
             <div class="centered-content vision round">
-                <h3>Vision</h3>
+                <h2>Vision</h2>
                 <p>That everyone has a right to social inclusion, equally and wholly. Yarra Valley Support Coordination will support people of all abilities to achieve this and more.</p>
             </div>
         </div>
         <div class="row">
-            <img src="<?php echo get_template_directory_uri();?>/assets/images/iso.png" alt="Mission" class="round">
+            <img src="http://yvsc.test/wp-content/uploads/2023/11/stock-photo-portrait-of-happy-women-with-disability-on-spring-lawn-375512857.jpg" alt="Mission" class="round">
             <div class="centered-content values round">
-                <h3>values</h3>
+                <h2>Values</h2>
                 <p>To work with and alongside people to support them to fulfil their goals to enjoy their life. We are driven with a person-centred passion to contribute and make a difference in issues that matter to the community, both individually and collectively.</p>
             </div>
             <div class="grey-block-right round"></div>
@@ -92,7 +92,7 @@
                 <h2><?php the_title(); ?></h2>
                 <?php the_content(); ?>
             </span>
-            <a href="#" class="btn">
+            <a href="#" class="btn btn-fwd">
                 View our services
             </a>
         </div>
--- partials/home-hero.php
+++ partials/home-hero.php
@@ -1,6 +1,7 @@
-<section id="home-hero" class="home-hero">
-    <?php the_post_thumbnail(); ?>
+<section id="home-hero" class="home-hero" style="background-image:url('<?php echo the_post_thumbnail_url(); ?>')">
+    
     <div class="container">
+        <?php the_post_thumbnail( ); ?>
         <div class="white-trans-block round">
             <?php the_content(); ?>
         </div>
