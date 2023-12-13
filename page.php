f3f7a12a9cc9ab105ae3be1f5e891d76ef6f36af
Pablo Sazo <pablo@glooadvertising.com.au>
2023-12-13 11:21:12 +1100

    ASC

--- page.php
+++ page.php
@@ -125,7 +125,8 @@
             $args = array(
                 'post_parent' => $post->ID,
                 'post_type' => 'page',
-                'orderby' => 'menu_order'
+                'orderby' => 'menu_order',
+                'order' => 'ASC'
             );
 
             $child_query = new WP_Query( $args );
