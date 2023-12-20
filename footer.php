ad82fa03c86741e4077f3770c434adee965d2869
Pablo Sazo <pablo@glooadvertising.com.au>
2023-12-20 13:34:25 +1100

    Footer

--- footer.php
+++ footer.php
@@ -14,15 +14,22 @@
                     </a>
                     <div class="contact-method">
                         <h3><?php bloginfo( 'title' ); ?></h3>
-                        <p>Specialist Support Coordination<br><br> Registered NDIS Provider.Provider No: 4050110593</p>
+                        <p>Specialist Support Coordination<br> Registered NDIS Provider.Provider No: 4050110593</p>
                     </div>
                 </div>
                 <div class="col-right">
                     <div class="contact-method">
                         <h3>Location</h3>
-                        <a href="https://www.google.com/maps/search/Suite+3,+5+Church+Street+Healesville+VIC+3777+2F+John+St,+Lilydale+3140/@-37.7059082,145.2634998,11z/data=!3m1!4b1?entry=ttu" target="_blank" rel="noopener noreferrer" >
-                            <p>Suite 3, 5 Church Street Healesville VIC 3777<br><br> 2F John St, Lilydale 3140</p>
-                        </a>
+                        <p>
+                            <a href="https://www.google.com/maps/search/Suite+3,+5+Church+Street+Healesville+VIC+3777+2F+John+St,+Lilydale+3140/@-37.7059082,145.2634998,11z/data=!3m1!4b1?entry=ttu" target="_blank" rel="noopener noreferrer" >
+                                Suite 3, 5 Church Street Healesville VIC 3777
+                            </a>
+                        </p>
+                        <p>
+                            <a href="https://www.google.com/maps/place/2F+John+St,+Lilydale+VIC+3140/data=!4m2!3m1!1s0x6ad62e43faa0d8cb:0x4209bd4b3927b3e6?sa=X&ved=2ahUKEwiQ4LeF_JyDAxXrbGwGHRz5CV4Q8gF6BAgKEAA" target="_blank" rel="noopener noreferrer" >
+                                2F John St, Lilydale 3140
+                            </a>
+                        </p>
                     </div>
                     <div class="contact-method">
                         <h3>Phone</h3>
