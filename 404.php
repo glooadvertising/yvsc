96f1607594b371ac27d4b91dda2e7ecc460c48bb
Pablo Sazo <pablo@glooadvertising.com.au>
2024-01-22 14:30:03 +1100

    404 page

---
+++ 404.php
@@ -0,0 +1,27 @@
+<?php get_header();?>
+
+<section class="page-hero">
+        <div class="bg-figures">
+            <div class="left-figure" style="background-color:#81298F;"></div>
+            <div class="right-figure"></div>
+        </div>
+        <div class="container-narrow">
+            <div class="col-left">
+                <h1 style="color:#81298F;">We are sorry</h1>
+            </div>
+            <div class="col-right">
+                <img src="<?php echo get_template_directory_uri(  ); ?>/assets/images/error.png" alt="" class="round">
+            </div>
+        </div>
+    </section>
+
+<section class="standard-page-main"> 
+    <div class="container-narrow">
+        <div class="col-left">
+            <div>
+                <p>The page you are looking for doesnt exist.</p>
+            </div>
+        </div>
+    </section>
+<?php get_footer()?>
+
